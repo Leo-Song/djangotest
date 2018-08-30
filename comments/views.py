41ab5a3eafef3e2ea0892080be9b0fb1e2aaa948
Leo-Song <leonidss@outlook.com>
2018-08-30 17:21:04 +0800

    test

--- comments/views.py
+++ comments/views.py
@@ -53,3 +53,4 @@ def post_comment(request, post_pk):
             return render(request, 'Janblog/detail.html', context=context)
     # 不是 post 请求，说明用户没有提交数据，重定向到文章详情页。
     return redirect(post)
+####aa
