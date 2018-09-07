e0587ca3d0cbdf2576e4dd197e591ecbfc654930
Leo-Song <Leonidss@outlook.com>
2018-09-07 10:16:14 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,10 +2,8 @@
 import os
 import sys
 
-<<<<<<< HEAD
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LeoBlog.settings')
-=======
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LeoBlog.settings")
     try:
