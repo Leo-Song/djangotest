f3f1194c1424f9283820db34cda2e7f57e28d51d
Leo-Song <leonidss@outlook.com>
2018-09-07 14:23:43 +0800

    fabric file commit

--- LeoBlog/settings.py
+++ LeoBlog/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6y0mw9(-#^m0ceaskd0e_%l=t6qavnrav%2bok3#br%^81y&$t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+#DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
@@ -132,4 +132,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
