c6c74be0d68255d80a58581ce6f9ed314aa50f9d
Leo-Song <leonidss@outlook.com>
2018-09-14 17:56:48 +0800

    normal comment

--- Janblog/models.py
+++ Janblog/models.py
@@ -44,6 +44,10 @@ class Article(models.Model):
     def get_absolute_url(self):
         return reverse('Janblog:detail', kwargs={'pk': self.pk})
 
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
     class Meta:
         ordering = ['-last_modified_time']
 
--- Janblog/views.py
+++ Janblog/views.py
@@ -5,6 +5,7 @@ from comments.forms import CommentForm
 # Create your views here.
 from .models import Article, Category
 import markdown
+from django.views.generic import ListView
 
 def index(request):
     post_list = Article.objects.all()
@@ -12,6 +13,9 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Article, pk=pk)
+
+    #浏览数+1
+    post.increase_views()
     post.body = mark_safe(markdown.markdown(post.body,
                                   extensions=[
                                      'markdown.extensions.extra',
