d5155454cd489ced6b66d645f1ed9cb2a512bd3e
Leo-Song <leonidss@outlook.com>
2018-10-25 10:10:36 +0800

    complete require

--- Janblog/urls.py
+++ Janblog/urls.py
@@ -5,8 +5,8 @@ from Janblog import views
 app_name = 'Janblog'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', views.IndexView.as_view(),name='index'),
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(),name='index'),
+    # path('', views.index, name='index'),
     # re_path('^search/$',views.search, name='search'),
     re_path('^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
     re_path('^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchivesView.as_view(), name='archives'),
